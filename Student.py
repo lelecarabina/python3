d09ea81490984c98d17c6dd9e5b48b4403e04966
Leticia B <lelecarabina@users.noreply.github.com>
2019-05-16 10:52:46 -0400

    Create Student.py
    Class file for StudentFinder.py.
    Task: write a program that searches an object of Student for students' names starting with a specific letter then prints the results.

---
+++ Student.py
@@ -0,0 +1,33 @@
+#Filename Student.java
+#Written by Lelecarabina
+#Written on Jul 14th, 2017
+#Class #12299 - Lesson 10
+#Re-written on 5/15/2019
+
+class Student: 
+
+    global name
+    global id
+    global gpa
+
+    # constructor
+    def __init__(self, name, id, gpa):
+        self.name = name
+        self.id = id
+        self.gpa = gpa
+        
+    
+    #get name value to main()
+    def getName(self):
+        return self.name
+        
+        
+    #get ID value to main()
+    def getId(self):
+        return self.id
+        
+ 
+    #get GPA value to main()
+    def getGpa(self):
+        return self.gpa
+            
