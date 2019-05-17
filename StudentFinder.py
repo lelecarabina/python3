f6d854163349d6820c35284d5988325184eec152
Leticia B <lelecarabina@users.noreply.github.com>
2019-05-17 11:27:36 -0400

    Update StudentFinder.py
    Final solution.

--- StudentFinder.py
+++ StudentFinder.py
@@ -1,5 +1,5 @@
 #Filename StudentFinder.java
-#Written by Lele B.
+#Written by Lele Carabina
 #Written on Jul 14th, 2017
 #Class #12299 - Lesson 10
 #Re-written on 5/15/2019
@@ -42,54 +42,64 @@ names = ["Albert Einstein",
         "Werner Karl Heisenberg",
         "Wilhelm Conrad Roentgen"]
 
-#vars used in object's initialization loop 
-gpasLen = len(gpas)
-numStudents = len(names)
 
-#random number
-rand = int(((random.random() * gpasLen) + gpasLen) % gpasLen)
-
-#Create new Student object students
-students = [Student((),(),())] * numStudents
-
-print("All Students:")
-
-# Initialize List of objects Student students with **CONSTRUCTOR** and Print original object students
-for x in range(numStudents): 
-    #name = from array 'names' id = incremental 1101 + x gpa = rand #, wraps around gpas length to restrict vals to only gpas' vals
-    students[x] = (Student(names[x], (1101 + x), (gpas[rand]))) # constructor
-    print(students[x].getName(), students[x].getId(), students[x].getGpa())
+#Print method
+def contactPrint(arr):
+    for x in range(len(arr)): 
+        print("[{}] Name: {} - ID: {} - GPA: {}".format(x, arr[x].getName(), arr[x].getId(), arr[x].getGpa()))
+    # end for loop
+# end contactPrint()
 
 
-#prompt user for input
-key = input("\nEnter the first letter of student's name: ")
-
-#filter(fulfills_some_condition, lst)
-#studentsNames = filter(lambda x: x.startswith(key), students)
-#OR:
-for x in range(len(students)):
-    studentsNames = [item for item in students if students[x].name[0] == key]   
-    #print(studentsNames)
-
-#print sorted list header
-print("Students names starting with '{}'".format(key.upper()))    
-
-#loop through sorted list
-for x in range(numStudents): 
-    #end loop if no student's names matches user's input
-    if studentsNames == "": 
-        print("NOT FOUND")
-        break
-    # end if statement
+def main():
+    #vars used in object's initialization loop 
+    gpasLen = len(gpas)
+    numStudents = len(names)
+    rand = int(((random.random() * gpasLen) + gpasLen) % gpasLen) #random number to select gpas from list
+    #store students' names starting with searching key
+    studentsNames = []
     
-    #print sorted list is student's name matches user's input, along with name's id and gpa
-    for name in studentsNames:  
-        if name == students[x].name:
-            print("     Name: {} -> ID: {} -> GPA: {}".format(students[x].name, students[x].id, students[x].gpa))
-            
-        # end if statement            
-    # end for each loop            
-# end for loop
+    #Create new Student object students
+    students = [Student((),(),())] * numStudents
+    
+    # Initialize List of objects Student students with **CONSTRUCTOR** and Print original object students
+    print("All Students:")
+    for x in range(numStudents): 
+        #'name' from array 'names', 'id' incremental 1101 + x, 'gpa' rand number that wraps around gpas length to restrict vals to only gpas' vals
+        students[x] = Student(names[x], 1101 + x, gpas[rand])# constructor
+    
+    #print students list
+    contactPrint(students)
+    
+    #prompt user for input
+    key = input("\nEnter the first letter of student's name: ")
+    
+    #look for key in students.name list...
+    for x in range(len(students)):
+        #if key found...
+        if students[x].name[0] == key.upper(): 
+            #add student record to studentsNames list...
+            studentsNames.append(students[x].name)               
+    
+    #loop through sorted list
+    print("Students names starting with '{}'".format(key.upper()))
+    for x in range(numStudents): 
+        #end loop if no student's names matches key
+        if studentsNames == "": 
+            print("NOT FOUND")
+            break
+        # end if statement
+        
+        #print student records that matched key
+        for name in studentsNames:  
+            if name == students[x].name:
+                print("     Name: {} -> ID: {} -> GPA: {}".format(students[x].name, students[x].id, students[x].gpa))    
+            # end if statement            
+        # end for each loop            
+    # end for loop
+    
+    #message to user, ending program
+    print("Program Ended.")
 
-#message to user, ending program
-print("Program Ended.")
+#call MAIN()
+main()
