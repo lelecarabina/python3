b0af02ada129c050f2d9138a32eac47f0fe2e89f
Leticia B <lelecarabina@users.noreply.github.com>
2020-08-11 18:37:40 -0400

    Update clipboard_to_file_no-loop.py

--- clipboard_to_file_no-loop.py
+++ clipboard_to_file_no-loop.py
@@ -4,18 +4,18 @@ import pyperclip
 from datetime import datetime
 
 try:
-    t = datetime.now()
-    tformatted = t.strftime("%m/%d/%Y, %H:%M:%S")
+    t = datetime.now() # catch sate and time
+    tformatted = t.strftime("%m/%d/%Y, %H:%M:%S") #format date/time string
     print(tformatted + '\nCopying text...') # Feedback
-    save_path = '/Users/Lel/Dropbox/LeDesignPro/Leticia/Texts'
-    file = os.path.join(save_path, 'le_python_clipboard.txt')     
-    f= open(file, 'a')
-    text = pyperclip.paste() # content of clipboard
+    save_path = '/Users/Lel/Dropbox/LeDesignPro/Leticia/Texts' #path to generated file
+    file = os.path.join(save_path, 'le_python_clipboard.txt') #generated file's name    
+    f= open(file, 'a') #open file for appending
+    text = pyperclip.paste() # catch clipboard content
     print('> "' + text + '"\n') #Feedback
-    f.write(tformatted + '\n' + text + '\n\n')
-    f.close()
+    f.write(tformatted + '\n' + text + '\n\n') #write  clipboard content to file
+    f.close() #close file
     print('Copy completed!\n---') #Feedback
-    sys.exit(0)
+    sys.exit(0) #end python program
 
-except Exception as error:
-    print(error)
+except Exception as error: #catch error
+    print(error) #print error
