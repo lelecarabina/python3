69bd285268199efd9f9a41bccf1753522c997305
Leticia B <lelecarabina@users.noreply.github.com>
2020-08-11 18:24:54 -0400

    Add files via upload
    Capture clipboard content and save to separate file with date/time stamps.

---
+++ clipboard_to_file_no-loop.py
@@ -0,0 +1,23 @@
+import sys
+import os.path
+import keyboard
+import pyperclip
+from datetime import datetime
+
+try:
+    x = 0
+    t = datetime.now()
+    tformatted = t.strftime("%m/%d/%Y, %H:%M:%S")
+    print(tformatted + '\nCopying text...[' + str(x) + ']') # Feedback
+    save_path = '/Users/Lel/Dropbox/LeDesignPro/Leticia/Texts'
+    file = os.path.join(save_path, 'le_python_clipboard.txt')     
+    f= open(file, 'a')
+    text = pyperclip.paste() # content of clipboard
+    print('> "' + text + '"\n') #Feedback
+    f.write(tformatted + '\n' + text + '\n\n')
+    f.close()
+    print('Copy completed!\n---') #Feedback
+    sys.exit(0)
+
+except Exception as error:
+    print(error)
